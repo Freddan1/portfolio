13050b913d12b88bff3f2b99eb7a167b25036778
Fredrik <f.eriksson86@gmail.com>
2024-12-09 15:59:49 +0100

    Took assistant away

--- app/page.tsx
+++ app/page.tsx
@@ -12,50 +12,50 @@ import { useEffect } from "react";
 
 export default function Home() {
 
-  useEffect(() => {
-    const iframe = document.createElement("iframe");
+  // useEffect(() => {
+  //   const iframe = document.createElement("iframe");
 
-    const iframeStyles = (styleString: string) => {
-      const style = document.createElement("style");
-      style.textContent = styleString;
-      document.head.append(style);
-    };
+  //   const iframeStyles = (styleString: string) => {
+  //     const style = document.createElement("style");
+  //     style.textContent = styleString;
+  //     document.head.append(style);
+  //   };
 
-    iframeStyles(`
-      .chat-frame {
-        position: fixed;
-        bottom: 50px;
-        right: 50px;
-        width: 300px;
-        height: 400px;
-        border: none;
-        z-index: 1000;
-      }
-    `);
+  //   iframeStyles(`
+  //     .chat-frame {
+  //       position: fixed;
+  //       bottom: 50px;
+  //       right: 50px;
+  //       width: 300px;
+  //       height: 400px;
+  //       border: none;
+  //       z-index: 1000;
+  //     }
+  //   `);
 
-    iframe.src = "http://localhost:3000/chatbot";
-    iframe.classList.add("chat-frame");
-    document.body.appendChild(iframe);
+  //   iframe.src = "http://localhost:3000/chatbot";
+  //   iframe.classList.add("chat-frame");
+  //   document.body.appendChild(iframe);
 
-    const handleMessage = (e: MessageEvent) => {
-      if (e.origin !== "http://localhost:3000") return;
-      const dimensions = JSON.parse(e.data);
-      iframe.width = dimensions.width;
-      iframe.height = dimensions.height;
-      iframe.contentWindow?.postMessage(
-        "111fa404-5936-4212-a676-d7087f5b212f",
-        "http://localhost:3000/"
-      );
-    };
+  //   const handleMessage = (e: MessageEvent) => {
+  //     if (e.origin !== "http://localhost:3000") return;
+  //     const dimensions = JSON.parse(e.data);
+  //     iframe.width = dimensions.width;
+  //     iframe.height = dimensions.height;
+  //     iframe.contentWindow?.postMessage(
+  //       "111fa404-5936-4212-a676-d7087f5b212f",
+  //       "http://localhost:3000/"
+  //     );
+  //   };
 
-    window.addEventListener("message", handleMessage);
+  //   window.addEventListener("message", handleMessage);
 
-    // Cleanup on component unmount
-    return () => {
-      window.removeEventListener("message", handleMessage);
-      document.body.removeChild(iframe);
-    };
-  }, []);
+  //   // Cleanup on component unmount
+  //   return () => {
+  //     window.removeEventListener("message", handleMessage);
+  //     document.body.removeChild(iframe);
+  //   };
+  // }, []);
 
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
